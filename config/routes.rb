d2b9361a09f36a746937fcde2773032021233cc9
Tomas Makaj <Tmakajcoding@gmail.com>
2022-09-20 16:24:32 -0400

    commit tree 456869c5529f6bf50c582f98eae1689f8c5cde7b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # resources :spices
+  
+  resources :spices, only: [:index, :update, :destroy, :create]
+  
+  # get 'spices/index'
+  # get 'spices/show'
+  # get 'spices/create'
+  # get 'spices', to: 'spices#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
